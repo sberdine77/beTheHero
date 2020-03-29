f042da8836088d31ae6b68de1c3eadb2e264c1ae
raquel <sbsx77xavier@hotmail.com>
2020-03-29 18:39:06 -0300

    Frontend integration with backend. Api requests to list data, login, logout, register

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
 import logoImage from '../../assets/logo.svg';
 import heroesImage from '../../assets/heroes.png';
+import api from '../../services/api';
 
 export default function Logon() {
+
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', {id});
+            
+            localStorage.setItem('ngoId', id);
+            localStorage.setItem('ngoName', response.data.name);
+            history.push('/profile')
+        } catch (error) {
+            alert('Falha no login. Tente novamente.');
+        }
+    }
+
     return(
         <div className="logon-container">
             <section className="form">
                 <img src={logoImage} alt="Be The Hero Logo"/>
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
-                    <input placeholder="Sua ID"></input>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={ e => setId(e.target.value) }
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
